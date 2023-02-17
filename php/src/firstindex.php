d40098d5159e5e1be661a5cc24e792ddf043a430
inose-yuudai <inobee1412@yahoo.co.jp>
2023-02-17 12:19:33 +0900

    必要なやつを持ってきた

---
+++ php/src/firstindex.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+
+if(isset($_SESSION['id'])) {
+  header('Location: ../admin/auth/signin.php');
+  exit;
+}
+
+if(isset($_POST['submit'])) {
+  header('Location: ../admin/auth/signin.php');
+  exit;
+}
+print $_SESSION['id'];
+?>
+<form action="" method="post">
+  <input type="submit" name="submit" value="ログイン">
+</form>
